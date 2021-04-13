52a4b23aed5f9e5041a53db0c49bc270c301c118
declanyoung <declanyoung23@gmail.com>
2021-04-13 21:36:56 +0800

    Create home-page.php

---
+++ home-page.php
@@ -0,0 +1,39 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+    ?>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+<section class="container-fluid aboutbg text-center">
+      
+                
+        <div class="blocker">
+        
+        
+        </div>
+      
+
+      <section class="container-fluid articlebg">
+      <div class="container">
+          <div class="row"><!-- a row that gives access to s columns-->
+          </div>
+          <h3 class="text-center article-title">LATEST ARTICLES</h3>
+          </div>
+          
+          <div class="col-md-4 article-cent">
+              
+              
+   
+          </div>
+          
+              <div class="col-md-4 article-cent">
+         
+          </div>
+
+          
+                            <div class="col-md-4 article-cent">
+            </div>
+      
+</section>
+<!-- container-fluid-->
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
