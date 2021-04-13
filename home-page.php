66b8d08eb77c553cacccfcd3088a045ddb6f1317
declanyoung <declanyoung23@gmail.com>
2021-04-13 21:43:15 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -6,34 +6,44 @@
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
 <section class="container-fluid aboutbg text-center">
       
+    <div class="=container">
                 
-        <div class="blocker">
+        <div class="blocker">  </div>
         
         
         </div>
-      
+    
+</section>
+
 
       <section class="container-fluid articlebg">
       <div class="container">
-          <div class="row"><!-- a row that gives access to s columns-->
-          </div>
+          <div class="row">
+              <!-- a row that gives access to s columns-->
+          
+          
+          <div>
           <h3 class="text-center article-title">LATEST ARTICLES</h3>
           </div>
           
           <div class="col-md-4 article-cent">
-              
-              
    
           </div>
           
-              <div class="col-md-4 article-cent">
+          
+         <div class="col-md-4 article-cent">
          
           </div>
 
           
-                            <div class="col-md-4 article-cent">
-            </div>
+         <div class="col-md-4 article-cent">
+             
+          </div>
       
+          </div>
+        
+          </div>
+    
 </section>
 <!-- container-fluid-->
 <?php get_footer(); /* Tells WordPress to include footer.php */ ?>
